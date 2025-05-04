1fb6ad6138093fc3727e3d4030c6e5bf21978a33
cornelsi <crnlswekesa@gmail.com>
2025-05-04 16:33:30 +0300

    week 7 assignment done

---
+++ answers.sql
@@ -0,0 +1,47 @@
+-- Question 1 Achieving 1NF (First Normal Form) 🛠️
+CREATE TABLE Orders (
+  OrderID INT PRIMARY KEY,
+  CustomerName VARCHAR(100)
+);
+
+CREATE TABLE Products (
+  OrderID INT,
+  Product VARCHAR(100),
+  FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+INSERT INTO Orders (OrderID, CustomerName) VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+INSERT INTO Products (OrderID, Product) VALUES
+(101, 'Laptop'),
+(101, 'Mouse'),
+(102, 'Tablet'),
+(102, 'Keyboard'),
+(102, 'Mouse'),
+(103, 'Phone');
+
+
+-- Question 2 Achieving 2NF (Second Normal Form) 🧩
+CREATE TABLE Orders (
+  OrderID INT PRIMARY KEY,
+  CustomerName VARCHAR(100)
+);
+
+CREATE TABLE OrderDetails_2NF (
+  OrderID INT,
+  Product VARCHAR(100),
+  Quantity INT,
+  PRIMARY KEY (OrderID, Product),
+  FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+INSERT INTO OrderDetails_2NF (OrderID, Product, Quantity) VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
